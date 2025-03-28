83c3aab0b7baa0e0f83e5e6882e3928c23c54298
Pete Cole <pcole@meadroid.com>
2025-03-28 10:18:49 +0000

    use webpack to create bundles

---
+++ configs/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const bundleConfig = require('o:/MeadCo/Dev/ScriptX.Print.Client/configs/distbundlesconfig.json');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// Generate a webpack config for each bundle in distbundlesconfig.json
+module.exports = bundleConfig.map(bundle => {
+    // Extract the output filename and directory
+    const outputPath = path.dirname(bundle.outputFileName);
+    const outputFilename = path.basename(bundle.outputFileName);
+
+    return {
+        mode: 'production',
+        entry: bundle.inputFiles,
+        output: {
+            path: path.resolve(__dirname, outputPath),
+            filename: outputFilename,
+            library: {
+                name: 'MeadCo',
+                type: 'window',
+                export: 'default'
+            }
+        },
+        devtool: 'source-map',
+        optimization: {
+            minimize: true
+        },
+        plugins: [
+            new CleanWebpackPlugin({
+                cleanOnceBeforeBuildPatterns: [],
+                cleanAfterEveryBuildPatterns: ['webpack-entries']
+            })
+        ]
+    };
+});
